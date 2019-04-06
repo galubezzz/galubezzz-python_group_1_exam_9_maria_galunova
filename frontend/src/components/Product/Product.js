ea3e0f869e5bc602419a5ab933ff90bac33cdb5b
galubezzz <galubezzz@gmail.com>
2019-04-06 02:56:27 -0700

    add a photo display to Product

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -9,10 +9,14 @@ class Product extends Component {
         if (this.props.product && this.props.product.categories){
             const categories =  this.props.product.categories.map(category => {return category.name})
         }
+        let photo = "http://localhost:8000/uploads/no_image.png";
+        if (this.props.product.photos[0]) {
+            photo = this.props.product.photos[0].photo
+        }
         const link = "/products/" + this.props.product.id;
         return (
             <div className="card m-4" style={{"width": "18rem"}}>
-                <img src={this.props.product.name} className="card-img-top" alt="..."/>
+                <img src={photo} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <NavLink className="nav-link card-title h5 text-dark p-0"
                              to={link}>{this.props.product.name}</NavLink>
